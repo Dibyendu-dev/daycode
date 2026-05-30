2ef1d255f890515a4b5485cd927a252e1ef5c02f
ddas4548 <ddas4548@gmail.com>
2026-05-30 10:35:47 +0530

    bug fixes

--- packages/cli/src/lib/http-errors.ts
+++ packages/cli/src/lib/http-errors.ts
@@ -6,7 +6,7 @@ type ErrorResponse = {
 
 export async function getErrorMessage(response:ErrorResponse) {
     try {
-        const data = (await Response.json()) as {error?: string};
+        const data = (await response.json()) as {error?: string};
         if (typeof data.error === "string" && data.error.length > 0) {
             return data.error
         }
